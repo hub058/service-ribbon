a0bd3aa17baf77b2f59f70a274c12d4413044230
endon058 <877058128@qq.com>
2018-06-23 12:14:21 +0800

    SpringCloud第三部分 服务消费者(RestTemplate-Ribbon) 添加熔断器-仪表盘

--- src/main/java/com/example/demo/HelloService.java
+++ src/main/java/com/example/demo/HelloService.java
@@ -32,13 +32,4 @@ public class HelloService {
         return "hi,"+name+",sorry,error!";
     }
 
-    // TODO 4:启动：service-ribbon 工程
-    /*
-    启动：service-ribbon 工程，当我们访问http://localhost:8764/hi?name=forezp,浏览器显示：
-    hi forezp,i am from port:8762
-    此时关闭 service-hi 工程，当我们再访问http://localhost:8764/hi?name=forezp，浏览器会显示：
-    hi ,forezp,orry,error!
-    这就说明当 service-hi 工程不可用的时候，service-ribbon调用 service-hi的API接口时，
-    会执行快速失败，直接返回一组字符串，而不是等待响应超时，这很好的控制了容器的线程阻塞。
-    */
 }
--- src/main/java/com/example/demo/ServiceRibbonApplication.java
+++ src/main/java/com/example/demo/ServiceRibbonApplication.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,6 +27,10 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 // TODO 2:在程序的启动类加@EnableHystrix注解开启Hystrix
 @EnableHystrix
+// TODO 22:在主程序启动类中加入@EnableHystrixDashboard注解，开启hystrixDashboard
+// 打开浏览器：访问http://localhost:8764/hystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServiceRibbonApplication {
 
 	public static void main(String[] args) {
@@ -33,5 +41,16 @@ public class ServiceRibbonApplication {
     @LoadBalanced
     RestTemplate restTemplate(){
 	    return new RestTemplate();
+    }
+
+    // 在Springboot2.0以上版本中需要添加这样一个Servlet
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
